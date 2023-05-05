afaa6194c28e39470dee83fe33d90473a2805124
Uthaeus <romanlavery@gmail.com>
2023-05-05 11:25:57 -0600

    added validation to user model, created nav component and stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', 
+    path_names: {  
+      sign_in: 'login', 
+      sign_out: 'logout', 
+      sign_up: 'register', 
+      edit: 'profile' 
+    }
+    
   resources :meetups
   
   get 'details', to: 'pages#details'
